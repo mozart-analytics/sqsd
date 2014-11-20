dc090bda052fdb38414e1f3acc74fbbefb1261be
ortiz.manuel@hp.com <ortiz.manuel@hp.com>
2014-11-20 18:23:10 -0400

    Refactored code and added Dockerfile and gitignore.

--- src/sqsd.groovy
+++ src/sqsd.groovy
@@ -4,19 +4,17 @@
  * <p>
  * Copyright (c) 2014 Mozart Analytics
  *
- * @author  <a href="mailto:ortiz.manuel@mozartanalytics.com">Manuel Ortiz</a>
  * @author  <a href="mailto:abdiel.aviles@mozartanalytics.com">Abdiel Aviles</a>
+ * @author  <a href="mailto:ortiz.manuel@mozartanalytics.com">Manuel Ortiz</a>
  */
 
+@Grab(group='com.amazonaws', module='aws-java-sdk', version='1.9.6')
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2')
 
 import groovy.json.JsonParserType
 import groovy.json.JsonSlurper
 import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
-
-@Grab(group='com.amazonaws', module='aws-java-sdk', version='1.9.6')
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
-
 import com.amazonaws.AmazonClientException
 import com.amazonaws.AmazonServiceException
 import com.amazonaws.auth.BasicAWSCredentials
@@ -27,72 +25,85 @@ import com.amazonaws.services.sqs.model.DeleteMessageRequest
 import com.amazonaws.services.sqs.model.Message
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest
 
+// TODO: Use Groovy's ConfigSlurper to enable configuration using properties file or env vars.
 /**
- * AWS SQS Configs
+ * AWS SQS  related configuration env-vars
  */
-def awsAccessKey = System.getenv("AWS_ACCESS_KEY_ID") ?: "yourKeyHere"
-def awsSecretKey = System.getenv("AWS_SECRET_KEY") ?: "yourSecretHere"
-def awsCreds = new BasicAWSCredentials(awsAccessKey as String, awsSecretKey as String)
+String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID")
+String AWS_SECRET_kEY = System.getenv("AWS_SECRET_KEY")
+String REGION_NAME = System.getenv("AWS_REGION_NAME") ?: "us-east-1"
+String SQS_QUEUE_NAME = System.getenv("SQS_QUEUE_NAME")
+String SQS_QUEUE_URL = System.getenv("SQS_QUEUE_URL")
+int MAX_NUMBER_OF_MESSAGES_PER_REQUEST = System.getenv("MAX_NUMBER_OF_MESSAGES") as Integer ?: 10
 
-def REGION_NAME = System.getenv("AWS_REGION_NAME") ?: "us-east-1"
-def TARGET_QUEUE = System.getenv("SQS_TARGET_QUEUE") ?: "yourSqsQueueName"
-def MAX_NUMBER_OF_MESSAGES_PER_REQUEST = System.getenv("MAX_NUMBER_OF_MESSAGES") as Integer ?: 10
+/**
+ * HTTP service related configuration env-vars
+ */
+String HTTP_HOST = System.getenv("HTTP_HOST") ?: "http://127.0.0.1:10000"
+String HTTP_PATH = System.getenv("HTTP_PATH") ?: "/comments-db-service/comments.json"
+String HTTP_REQUEST_CONTENT_TYPE = System.getenv("HTTP_REQUEST_CONTENT_TYPE") ?: "application/json"
 
-def LOCAL_ENDPOINT = System.getenv("LOCAL_ENDPOINT") ?: "/"
+// TODO: Assertions of required variables without defaults.
+// TODO: Print properties.
 
+// Setup sqs client.
+def awsCreds = new BasicAWSCredentials(AWS_ACCESS_KEY_ID as String, AWS_SECRET_kEY as String)
 def sqs = new AmazonSQSClient(awsCreds)
-sqs.setRegion(Region.getRegion(Regions.fromName(REGION_NAME as String)))
-def targetQueue = sqs.getQueueUrl(TARGET_QUEUE as String).getQueueUrl()
+def sqsRegion = Region.getRegion(Regions.fromName(REGION_NAME as String))
+sqs.setRegion(sqsRegion)
+String sqsQueueUrl = SQS_QUEUE_URL ?: sqs.getQueueUrl(SQS_QUEUE_NAME as String).getQueueUrl() // Use provided queue url or name (url has priority)
 
 try {
-
-    println("Receiving messages from " + targetQueue)
+    println("Receiving messages from " + sqsQueueUrl)
     println("Max Number of Messages : " + MAX_NUMBER_OF_MESSAGES_PER_REQUEST)
 
-    // Configure request
-    def receiveMessageRequest = new ReceiveMessageRequest(targetQueue)
-    receiveMessageRequest.setMaxNumberOfMessages(MAX_NUMBER_OF_MESSAGES_PER_REQUEST)
+    // Configure sqs request
+    def receiveMessageRequest = new ReceiveMessageRequest()
+            .withQueueUrl(sqsQueueUrl)
+            .withMaxNumberOfMessages(MAX_NUMBER_OF_MESSAGES_PER_REQUEST)
+            .withWaitTimeSeconds(20) // TODO: Is this necessary?
 
     // Consume queue until empty
     while(true){
         def messages = sqs.receiveMessage(receiveMessageRequest).getMessages()
-        println("Total Received Messages : " + messages.size())
+        println "Total Received Messages : " + messages.size()
 
         // Break when empty
         if(messages.size() <= 0) break
 
         for (Message message : messages) {
-            if(handleMessage(LOCAL_ENDPOINT, message)) {
+            if(handleMessage(HTTP_HOST, HTTP_PATH, HTTP_REQUEST_CONTENT_TYPE, message)) {
                 // If successful, delete the message
-                println("Deleting Message")
+                println("Deleting message...")
                 String messageReceiptHandle = message.getReceiptHandle()
-                sqs.deleteMessage(new DeleteMessageRequest(targetQueue, messageReceiptHandle))
+                sqs.deleteMessage(new DeleteMessageRequest(sqsQueueUrl, messageReceiptHandle))
             }
             else // TODO we can validate that the long polling timeout is considerably longer than the visibility timeout, if true then we can skip the exception
                 throw new Exception("Local Service Failure") // We should stop consuming here
         }
+
+        println "Done!!"
     }
 }
-catch (AmazonServiceException ase) { ase.printStackTrace() }
+catch (AmazonServiceException ase) { ase.printStackTrace() } // TODO: Research how to add log4j or something similar.
 catch (AmazonClientException ace) { ace.printStackTrace() }
 
-def handleMessage(String endpoint, Message message){
-    def localhost = System.getenv("LOCAL_HOST") ?: "http://127.0.0.1"
+def handleMessage(String httpHost, String httpPath, String contentType, Message message){
     def slurper = new JsonSlurper().setType(JsonParserType.LAX)
     def payload = slurper.parseText(message.getBody())
 
     int status
     try {
-        def resp = new RESTClient(localhost).post(
-                path : endpoint,
+        def resp = new RESTClient(httpHost).post(
+                path : httpPath,
                 body : payload,
-                contentType : System.getenv("MIME_TYPE") ?: "application/json"
+                contentType : contentType
         )
         status = resp.status
     }
     catch(HttpResponseException ex ) { status = ex.response.status }
 
-    println "POST " + localhost + endpoint + " :: " + status
+    println "POST " + httpHost + httpPath + " :: " + status // TODO: Find out the correct way of displaying this message.
 
     status < 400
 }
